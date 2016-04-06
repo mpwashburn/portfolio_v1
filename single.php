38f3aa8f78b34751610b0608b89b077708b5a301
Michael Washburn <washburn.michael@gmail.com>
2016-04-06 11:48:45 -0700

    added sidebar to project single page

--- single.php
+++ single.php
@@ -3,25 +3,28 @@
 <?php if (have_posts()) : ?>
 <?php while(have_posts()) : the_post(); ?>
 
-<section class="post">
-  <div class="container">
+  <div class="post container">
     <div class="row">
-      <!-- can be used for sidebar -->
-      <div class="col-md-2"></div>
-      <div class="col-md-8 content">
+      <div class="col-md-offset-1 col-md-8 content">
        <div class="card">
           <?php if ( has_post_thumbnail() ) : ?>
               <?php the_post_thumbnail('large'); ?>
           <?php endif; ?>
-          <div class="card-content-post">
+          <div class="card-content">
             <h2><?php the_title(); ?> </h2>
-            <p class="text-content-post"><?php the_content(); ?></p>
+            <p class=""><?php the_content(); ?></p>
           </div>
+        </div>
+      </div>
+      <!-- can be used for sidebar -->
+      <div class="col-md-2">
+        <div class="sidebar">
+          <h3>sidebar</h3>
+          <a href="#">Github</a>
         </div>
       </div>
     </div>
   </div>
-</section>
 
 <?php endwhile; ?>
 <?php endif; ?>
