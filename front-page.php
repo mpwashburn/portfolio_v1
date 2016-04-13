2f9cc09d8842dedee81944d380bdbdf6bd7d6cb2
Michael Washburn <washburn.michael@gmail.com>
2016-04-13 16:44:19 -0700

    changd front-page styling, and updated project/post page styling

--- front-page.php
+++ front-page.php
@@ -3,47 +3,61 @@
   <div class="row">
     <div class="col-md-offset-6 col-md-6">
       <div id="jumbo-text">
-        <h1>An Adventure as a Web Developer</h1>
+        <h1>Web Developer</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sunt excepturi architecto atque inventore hic a natus quisquam culpa molestiae temporibus officia quidem autem porro debitis cum, expedita pariatur quis.</p>
       </div>
     </div>
   </div>
 </section>
 <section class="page-break">
-  <h1>projects</h1>
+  <h1>Projects</h1>
 </section>
 <div class="projects container">
-<h2>Newest Projects</h2>
 <div class="row">
-  <section class="col-md-4">
+  <section class="col-md-6">
     <div class="card">
-      <a href=""><img class="icon img-responsive" src="<?php echo get_template_directory_uri() ?>/img/Everest_Game_SS.jpg" alt="Icon"></a>
-      <div class="card-content">
-        <h3>Everest Trivia <small>HTML | CSS | Javascript</small></h3>
-        <p class="text-content">Everest is a simple trivia game that was developed in the first few weeks of code school at General Assembly. Inspired by my love of the outdoors and wanting to seek more through nature</p>
-        <p class="text-right"><a href="#" class="btn btn-danger">Read more</a></p>
+      <div class="img-bg">
+        <img class="icon img-responsive" src="http://placehold.it/350x200" alt="Icon">
+      </div>
+      <div class="content">
+        <h2>Title</h2>
+        <h3>Keywords</h3>
       </div>
     </div>
   </section>
 
-  <section class="col-md-4">
+  <section class="col-md-6">
     <div class="card">
-      <a href="#"><img class="icon img-responsive" src="<?php echo get_template_directory_uri() ?>/img/TTT.jpg" alt="Icon"></a>
-      <div class="card-content">
-        <h3>Tic-Tac-Toe <small>HTML | CSS | Javascript</small></h3>
-        <p class="text-content">The classic Tic-Tac-Toe game built in week 1 of code school at General Assembly</p>
-        <p class="text-right"><a href="#" class="btn btn-danger">Read more</a></p>
+      <div class="img-bg">
+        <img class="icon img-responsive" src="http://placehold.it/350x200" alt="Icon">
+      </div>
+      <div class="content">
+        <h2>Title</h2>
+        <h3>Keywords</h3>
       </div>
     </div>
   </section>
 
-  <section class="col-md-4">
+  <section class="col-md-6">
     <div class="card">
-      <a href="#"><img class="icon img-responsive" src="<?php echo get_template_directory_uri() ?>/img/portfolio_ss.jpg" alt="Icon"></a>
-      <div class="card-content">
-        <h3>Portfolio <small>Bootstrap | CSS | Wordpress</small></h3>
-        <p class="text-content">My portfolio site is my first project where I am working with custom wordpress themes. </p>
-        <p class="text-right"><a href="#" class="btn btn-danger">Read more</a></p>
+      <div class="img-bg">
+        <img class="icon img-responsive" src="http://placehold.it/350x200" alt="Icon">
+      </div>
+      <div class="content">
+        <h2>Title</h2>
+        <h3>Keywords</h3>
+      </div>
+    </div>
+  </section>
+
+  <section class="col-md-6">
+    <div class="card">
+      <div class="img-bg">
+        <img class="icon img-responsive" src="http://placehold.it/350x200" alt="Icon">
+      </div>
+      <div class="content">
+        <h2>Title</h2>
+        <h3>Keywords</h3>
       </div>
     </div>
   </section>
--- single.php
+++ single.php
@@ -5,7 +5,7 @@
 
   <div class="post container">
     <div class="row">
-      <div class="col-md-offset-1 col-md-8 content">
+      <div class="col-md-offset-2 col-md-8 content">
        <div class="card">
           <?php if ( has_post_thumbnail() ) : ?>
               <?php the_post_thumbnail('large'); ?>
@@ -13,16 +13,19 @@
           <div class="card-content">
             <h2><?php the_title(); ?> </h2>
             <p class=""><?php the_content(); ?></p>
+          <div class="social">
+            <a href="https://github.com/mpwashburn?tab=repositories" target="'newwindow'"> <i class="fa fa-github"></i></a>
+          </div>
           </div>
         </div>
       </div>
       <!-- can be used for sidebar -->
-      <div class="col-md-2">
+      <!-- <div class="col-md-2">
         <div class="sidebar">
           <h3>sidebar</h3>
           <a href="#">Github</a>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 
